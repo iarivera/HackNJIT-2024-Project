82c45f5f9eb2768a496b520d471835999b00ffa9
iarivera <iariveradev@outlook.com>
2024-11-02 16:45:02 -0400

    edited markdown and add basic game scene, and programmed basic player movement

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # HackNJIT-2024-Project
 
 ## This project is being made Maya Rivera and Max Hammond
-### Top candidates are either making a simple steampunk game in godot. Untitled Steampunk Game is a simple collect-athon, where you must collect 50 gears in under a minute to save your factory
+### Top candidates are either making a simple steampunk game in godot. Untitled Steampunk Game is a simple platformer to clear up insects in a machine you own
---
+++ untitled-steampunk-game/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+var SPEED = 400
+var JUMP = -600
+var screen_size 
+@onready var game = get_tree().get_root().get_node("Game")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP
+	
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
