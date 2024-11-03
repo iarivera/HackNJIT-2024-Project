4c1e611db80ca500d390bfad5cd3dc3aedeee2ae
iarivera <iariveradev@outlook.com>
2024-11-03 11:48:34 -0500

    animations added, and project exported

--- untitled-steampunk-game/game.gd
+++ untitled-steampunk-game/game.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 var gear1
+var gear2
+var gear3
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	gear1 = get_tree().current_scene.get_node("back_and_forth_gear")
-
+	gear2 = get_tree().current_scene.get_node("rotating_gear1")
+	gear3 = get_tree().current_scene.get_node("rotating_gear2")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- untitled-steampunk-game/player.gd
+++ untitled-steampunk-game/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-var SPEED = 400
+var SPEED = 200
 var JUMP = -600
-var FALL = 200
+var FALL = 300
 var screen_size 
+var state = 0
 @onready var game = get_tree().get_root().get_node("Game")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,14 +14,35 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	if Input.is_action_just_pressed("fast_fall") and not is_on_floor():
-		velocity.y = FALL
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP
+	if Input.is_action_just_pressed("fast_fall") and not is_on_floor():
+		velocity.y = FALL
 	
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if not is_on_floor():
+		if velocity.y > 0 and velocity.x > 0:
+			$AnimatedSprite2D.play("fall_right")
+		elif velocity.y > 0 and velocity.x < 0:
+			$AnimatedSprite2D.play("fall_left")
+		elif velocity.y < 0 and velocity.x > 0:
+			$AnimatedSprite2D.play("jump_right")
+		elif velocity.y < 0 and velocity.x < 0:
+			$AnimatedSprite2D.play("jump_left")
+		
+	if is_on_floor():
+		var direction := Input.get_axis("move_left", "move_right")
+		if direction:
+			velocity.x = direction * SPEED
+			if velocity.x > 0:
+				$AnimatedSprite2D.play("run_right")
+				state = 0
+			else:
+				$AnimatedSprite2D.play("run_left")
+				state = 1
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			if state == 0:
+				$AnimatedSprite2D.play("idle_right")
+			else:
+				$AnimatedSprite2D.play("idle_left")
 	move_and_slide()
